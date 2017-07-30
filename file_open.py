a088b895e2fde442ff17ac376e83451077d02fe4
Pvana <pavanyejare@gmail.com>
2017-07-30 13:18:24 +0530

    open file

---
+++ file_open.py
@@ -0,0 +1,11 @@
+
+
+f1 = "task.txt"
+f = open(f1,'a')
+f.write("1. attend the meetup \n")
+f.write("2. go home and sleep \n")
+f.write("3. ...... ")
+f.close()
+print ("File written successfully")
+	
+
